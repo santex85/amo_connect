cd2530a2fa8e079004969f7a186013b424d59226
Alexander Cheremikin <santex85@gmail.com>
2022-07-19 20:44:39 +0700

    Added trusted CSRF setting

--- amo_connect/settings.py
+++ amo_connect/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-CSRF_TRUSTED_ORIGINS = ['https://*.amo-connect.ru']
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUST").split(" ")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
