7338347ce226c5d7e71c90258ecb5f858836d05a
Alexander Cheremikin <santex85@gmail.com>
2022-07-20 12:03:52 +0700

    test app for prod

--- amo_connect/settings.py
+++ amo_connect/settings.py
@@ -19,15 +19,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+
+DEBUG = int(os.environ.get("DEBUG", default=1))
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", default='fsdffg4rg')
+if not DEBUG:
+    CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUST").split(" ")
 
-CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUST").split(" ")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
-
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+    ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
