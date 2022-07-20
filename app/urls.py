ac83e0c9a3426b61ba8995f70a10c02b323dec1c
Alexander Cheremikin <santex85@gmail.com>
2022-07-20 18:57:55 +0700

    test from amo

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from app.views import TestView, GetInfoView
+from app.views import TestView, GetInfoView, TestAmoView
 
 urlpatterns = [
     path('', TestView.as_view()),  # TODO сделать страницу заглушку
-    path('api/v1/', GetInfoView.as_view())
-
+    path('api/v1/', GetInfoView.as_view()),
+    path('api/v1/amo-test/', TestAmoView.as_view()),
 ]
