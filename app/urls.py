cb98ba6b76447f47a94351a75a5ccd516f72c094
Alexander Cheremikin <santex85@gmail.com>
2022-07-20 14:04:01 +0700

    test from bizon

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from app.views import TestView
+from app.views import TestView, GetInfoView
 
 urlpatterns = [
-    path('', TestView.as_view()),
+    path('', TestView.as_view()),  # TODO сделать страницу заглушку
+    path('api/v1/', GetInfoView.as_view())
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.views import View
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import requests
 
+
+# HbcGqxvFqH-e9M5xDY9BW-9z9ePFcSZMqzcewt9rW7cfcxPY9
 
 class TestView(View):
-    def get(self, request):
+    @staticmethod
+    def get(request):
         return render(request, 'test/test.html', context={'name': 'alex'})
+
+
+class GetInfoView(APIView):
+
+    @staticmethod
+    def post(request):
+        # headers = {
+        #     'X-Token': 'HbcGqxvFqH-e9M5xDY9BW-9z9ePFcSZMqzcewt9rW7cfcxPY9'
+        # }
+        # req = requests.get(
+        #     f'https://online.bizon365.ru/api/v1/webinars/reports/get?webinarId=Uverennostidengi',
+        #     headers=headers)
+        # print(req.text)
+        with open('test.txt', 'w') as file:
+            file.write(request.data)
+        return Response(status=status.HTTP_200_OK)
